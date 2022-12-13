d744e0c969a45d77df47a15f1850c39b1eb0e074
Matheus <matheusgaldinoinfo@gmail.com>
2022-12-13 15:49:25 -0300

    Subindo exercicio Read Json File

---
+++ ReadFile/Entity/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile.Entity
+{
+    public record struct  Cliente
+    {
+        public string nome { get; set; }
+        public string telefone { get; set;}
+
+    }
+}
---
+++ ReadFile/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ReadFile.Entity;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var path = @"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente1.json";
+
+        List<Cliente>clientes = new List<Cliente>();
+
+        for(var i = 2; i <= 6; i++)
+        {
+        using (StreamReader r = new StreamReader(path))
+        {
+            string json = r.ReadToEnd();
+            var cliente = JsonSerializer.Deserialize<Cliente>(json);
+                clientes.Add(cliente);
+        }
+            path = @$"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente{i}.json";
+        }
+            foreach(var cliente in clientes)
+            {
+                Console.WriteLine($"Nome: {cliente.nome} ||| Telefone: {cliente.telefone}");
+                Console.WriteLine("----------------------------------------------------");
+            }
+        
+
+
+    }
+}
