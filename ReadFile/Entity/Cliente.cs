60ebee07adc024a35ec3f5a8cde2c387bc8cf35d
Matheus <matheusgaldinoinfo@gmail.com>
2022-12-13 16:58:24 -0300

    Subindo codigo refatorado para utilização de recursividade

--- ReadFile/Entity/Cliente.cs
+++ ReadFile/Entity/Cliente.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReadFile.Entity
+﻿namespace ReadFile.Entity
 {
-    public record struct  Cliente
+    public record struct Cliente
     {
         public string nome { get; set; }
-        public string telefone { get; set;}
+        public string telefone { get; set; }
 
     }
 }
--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -1,36 +1,56 @@
-﻿
-
-using System;
+﻿using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using ReadFile.Entity;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-
-        var path = @"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente1.json";
-
-        List<Cliente>clientes = new List<Cliente>();
-
-        for(var i = 2; i <= 6; i++)
-        {
-        using (StreamReader r = new StreamReader(path))
-        {
-            string json = r.ReadToEnd();
-            var cliente = JsonSerializer.Deserialize<Cliente>(json);
-                clientes.Add(cliente);
-        }
-            path = @$"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente{i}.json";
-        }
-            foreach(var cliente in clientes)
-            {
-                Console.WriteLine($"Nome: {cliente.nome} ||| Telefone: {cliente.telefone}");
-                Console.WriteLine("----------------------------------------------------");
-            }
-        
-
+        Console.WriteLine("            Lista de clientes");
+        Console.WriteLine("---------------------------------------");
+        LerArquivos(1);
 
     }
+    private static void LerArquivos(int numeroArquivo)
+    {
+
+        var path = @$"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente{numeroArquivo}.json";
+
+        if (File.Exists(path))
+        {
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                var cliente = JsonSerializer.Deserialize<Cliente>(json);
+                Console.WriteLine($"Nome: {cliente.nome} ||| Telefone: {cliente.telefone}");
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+         path = @$"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente{numeroArquivo + 1}.json";
+        if (File.Exists(path))
+        {
+            LerArquivos(numeroArquivo + 1);
+        }
+    }
+
+//    var path = @"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente1.json";
+
+//    List<Cliente> clientes = new List<Cliente>();
+
+//        for(var i = 2; i <= 6; i++)
+//        {
+//        using (StreamReader r = new StreamReader(path))
+//        {
+//            string json = r.ReadToEnd();
+//    var cliente = JsonSerializer.Deserialize<Cliente>(json);
+//    clientes.Add(cliente);
+//        }
+//path = @$"C:\Users\I\source\repos\ReadFile\ReadFile\imports\cliente{i}.json";
+//        }
+//            foreach (var cliente in clientes)
+//{
+//    Console.WriteLine($"Nome: {cliente.nome} ||| Telefone: {cliente.telefone}");
+//    Console.WriteLine("----------------------------------------------------");
+//}
+        
 }
